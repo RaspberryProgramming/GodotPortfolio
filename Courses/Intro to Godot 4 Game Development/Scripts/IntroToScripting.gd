5d7330373427a76f93186d26ba5a51e3dd887f23
Camerin Figueroa <figueroa0609@protonmail.com>
2023-09-30 13:36:21 -0400

    Lesson 31 - Conditions

--- Courses/Intro to Godot 4 Game Development/Scripts/IntroToScripting.gd
+++ Courses/Intro to Godot 4 Game Development/Scripts/IntroToScripting.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
 var score : int = 5
-var move_speed : float = 2.53
-var game_over : bool = false
-var ability : String = "slash"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  game_over = true
-  print(game_over)
-  ability = "heal"
-  print(ability)
+  if score == 10:
+    print("You win!")
+  
+  if score > 5:
+    print("Score is greater than 5")
+    
+  var a : int = 50
+  var b : int = 100
 
+  if a < b:
+    print("a is less than b")  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
