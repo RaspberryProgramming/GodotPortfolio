3c364e0922a87d600836b378b432a3566ee597ff
Camerin Figueroa <figueroa0609@protonmail.com>
2023-09-30 14:07:35 -0400

    Lesson 42 - Coin Scene & code

---
+++ Courses/Intro to Godot 4 Game Development/Scripts/Coin.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _on_body_entered(body):
+  body.scale.x += 0.2
+  body.scale.y += 0.2
+  
+  queue_free()
--- Courses/Intro to Godot 4 Game Development/Scripts/Player.gd
+++ Courses/Intro to Godot 4 Game Development/Scripts/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var MOVE_SPEED : float = 200
+
 func _physics_process(delta):
   velocity.x = 0
   velocity.y = 0
@@ -16,6 +18,6 @@ func _physics_process(delta):
   if Input.is_key_pressed(KEY_DOWN):
     velocity.y += 1
 
-  velocity *= 50
+  velocity *= MOVE_SPEED
 
   move_and_slide()
