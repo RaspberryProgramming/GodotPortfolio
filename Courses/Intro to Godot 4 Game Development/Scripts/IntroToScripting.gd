76602a56cd65c0b94de3ea3a2b98289b3466a83e
Camerin Figueroa <figueroa0609@protonmail.com>
2023-09-30 13:32:57 -0400

    Lesson 24-25 - Intro to Scripting

--- Courses/Intro to Godot 4 Game Development/Scripts/IntroToScripting.gd
+++ Courses/Intro to Godot 4 Game Development/Scripts/IntroToScripting.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+var score : int = 5
+var move_speed : float = 2.53
+var game_over : bool = false
+var ability : String = "slash"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  game_over = true
+  print(game_over)
+  ability = "heal"
+  print(ability)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
