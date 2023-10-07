10d9ad9c63729bb1b9b1eaa1b8eaf3448c602001
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-07 12:59:16 -0400

    Balloon Popper - Lesson 6
    Added code to keep score and pop balloons

--- Courses/Godot 4 Mini-Projects/Balloon Popper/Balloon.gd
+++ Courses/Godot 4 Mini-Projects/Balloon Popper/Balloon.gd
@@ -8,4 +8,9 @@ var score_to_give : int = 1
 
 func _on_input_event(camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int):
   if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-    print("Increase Size")
+    scale += Vector3.ONE*size_increase
+    clicks_to_pop -= 1
+    
+    if clicks_to_pop == 0:
+      get_node("/root/Main").increase_score(score_to_give)
+      queue_free()
---
+++ Courses/Godot 4 Mini-Projects/Balloon Popper/BalloonManager.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+var score : int = 0
+
+func increase_score(amount:int):
+  score += amount
+  print(score)
