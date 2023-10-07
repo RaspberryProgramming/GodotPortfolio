e9a41425d3efdf9087c457bf1e2675dace8f2568
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-07 18:13:07 -0400

    Collision - Lesson 17
    Added collisions and trees. Resets scene when hitting a tree

--- Courses/Godot 4 Mini-Projects/Collision/PlayerMovement.gd
+++ Courses/Godot 4 Mini-Projects/Collision/PlayerMovement.gd
@@ -8,3 +8,8 @@ func _physics_process(delta):
     
   if Input.is_key_pressed(KEY_RIGHT):
     linear_velocity.x = move_speed
+
+
+func _on_body_entered(body):
+  if body.is_in_group("Tree"):
+    get_tree().reload_current_scene()
