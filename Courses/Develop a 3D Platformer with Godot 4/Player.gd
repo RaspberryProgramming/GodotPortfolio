50c10bebd92ea8ee70623a7906cc66c39459b36c
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-12 19:15:50 -0400

    Godot 3D Platformer - Lesson 6
    Adding player movement

--- Courses/Develop a 3D Platformer with Godot 4/Player.gd
+++ Courses/Develop a 3D Platformer with Godot 4/Player.gd
@@ -1,1 +1,17 @@
 extends CharacterBody3D
+
+var move_speed : float = 4.0
+var jump_force : float = 8.0
+var gravity : float = 20.0
+
+var facing_angle : float
+
+@onready var model : MeshInstance3D = get_node("Model")
+
+func _physics_process(delta):
+  var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+  var dir = Vector3(input.x, 0, input.y)
+  
+  velocity = dir
+  
+  move_and_slide()
