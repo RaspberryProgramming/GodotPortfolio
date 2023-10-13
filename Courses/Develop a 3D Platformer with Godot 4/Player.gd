4c6d2811157bdafefe71bc7133b5ec05e4872da2
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-13 18:19:10 -0400

    Godot 3D Platformer - Lesson 7
    Added character movement and gravity

--- Courses/Develop a 3D Platformer with Godot 4/Player.gd
+++ Courses/Develop a 3D Platformer with Godot 4/Player.gd
@@ -9,9 +9,20 @@ var facing_angle : float
 @onready var model : MeshInstance3D = get_node("Model")
 
 func _physics_process(delta):
+  
+  # Apply gravity
+  if not is_on_floor():
+    velocity.y -= gravity * delta
+    
   var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
   var dir = Vector3(input.x, 0, input.y)
   
-  velocity = dir
+  velocity.x = dir.x * move_speed
+  velocity.z = dir.z * move_speed
   
   move_and_slide()
+  
+  if input.length() > 0:
+    facing_angle = Vector2(input.y, input.x).angle()
+  
+  model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.125)
