3baead65380690b7590de74da3b721e11db2ed14
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-13 18:24:42 -0400

    Godot 3D Platformer - Lesson 8
    Add jump to player
    Add game over function and game over when falling off the edge

--- Courses/Develop a 3D Platformer with Godot 4/Player.gd
+++ Courses/Develop a 3D Platformer with Godot 4/Player.gd
@@ -13,16 +13,33 @@ func _physics_process(delta):
   # Apply gravity
   if not is_on_floor():
     velocity.y -= gravity * delta
-    
+  
+  # Apply Jump Velocity if jump key is pressed and on the floor
+  if Input.is_action_pressed("jump") and is_on_floor():
+    velocity.y = jump_force
+  
+  # Get keyboard input
   var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+  
+  # Convert to vector3
   var dir = Vector3(input.x, 0, input.y)
   
+  # Assign x/z velocity
   velocity.x = dir.x * move_speed
   velocity.z = dir.z * move_speed
   
+  # apply velocity to characterbody3d
   move_and_slide()
   
+  # if input is being added, change facing angle of player
   if input.length() > 0:
     facing_angle = Vector2(input.y, input.x).angle()
   
+  # Apply facing_angle to model's rotation
   model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.125)
+  
+  if global_position.y < -5:
+    game_over()
+
+func game_over():
+  get_tree().reload_current_scene()
