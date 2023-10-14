cda845ed2de7bcec203272a1c8c5e489a7999195
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-14 10:51:52 -0400

    Godot 3D Platformer - Lesson 15
    Adding score label and placing coins throughout the level

--- Courses/Develop a 3D Platformer with Godot 4/Player.gd
+++ Courses/Develop a 3D Platformer with Godot 4/Player.gd
@@ -9,6 +9,7 @@ var facing_angle : float
 var score : int
 
 @onready var model : MeshInstance3D = get_node("Model")
+@onready var score_text : Label = get_node("ScoreText")
 
 func _physics_process(delta):
   
@@ -48,3 +49,4 @@ func game_over():
 
 func add_score(amount):
   score += amount
+  score_text.text = str("Score: ", score)
