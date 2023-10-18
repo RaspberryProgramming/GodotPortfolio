efb8dd7fd33f27df8796a792d2b75197673b046e
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-18 19:14:57 -0400

    Turn-Based RPG - Lesson 15
    Connecting combat action buttons to player's combat actions
    Adding ability to submit combat action to turn manager from buttons

--- Courses/Build a Micro turn-Based RPG with Godot 4/CombatActionButton.gd
+++ Courses/Build a Micro turn-Based RPG with Godot 4/CombatActionButton.gd
@@ -3,4 +3,4 @@ extends Button
 var combat_action : CombatAction
 
 func _on_pressed():
-  get_node("/root/BattleScene").cur_character.cast_combat_action(combat_action)
+  get_node("/root/BattleScene").cur_char.cast_combat_action(combat_action)
--- Courses/Build a Micro turn-Based RPG with Godot 4/PlayerUI.gd
+++ Courses/Build a Micro turn-Based RPG with Godot 4/PlayerUI.gd
@@ -1,6 +1,6 @@
 extends VBoxContainer
 
-@export var buttons : Array
+@export var buttons : Array[NodePath]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,5 +10,19 @@ func _ready():
 func _on_character_begin_turn(character):
   visible = character.is_player
   
+  if character.is_player:
+    _display_combat_actions(character.combat_actions)
+  
 func _on_character_end_turn(character):
   visible = false
+
+func _display_combat_actions(combat_actions):
+  for i in len(buttons):
+    var button = get_node(buttons[i])
+    
+    if i < len(combat_actions):
+      button.visible = true
+      button.text = combat_actions[i].display_name
+      button.combat_action = combat_actions[i]
+    else:
+      button.visible = false
