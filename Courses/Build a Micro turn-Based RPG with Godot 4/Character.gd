dd8e5ff96e56cf87d650a0bdaba798f3b7d324ec
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-18 18:40:02 -0400

    Turn-based RPG - Lesson 9
    Added signals, added enemy and some more death logic

--- Courses/Build a Micro turn-Based RPG with Godot 4/Character.gd
+++ Courses/Build a Micro turn-Based RPG with Godot 4/Character.gd
@@ -11,8 +11,17 @@ class_name Character
 @onready var health_bar : ProgressBar = get_node("HealthBar")
 @onready var health_text : Label = get_node("HeathBar/HealthText")
 
+@export var visual : Texture2D
+@export var flip_visual : bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  
+  $Sprite.texture = visual
+  $Sprite.flip_h = flip_visual
+  
+  get_node("/root/BattleScene").character_begin_turn.connect(_on_character_begin_turn)
+  get_node("/root/BattleScene").character_end_turn.connect(_on_character_end_turn)
   health_bar.max_value = max_hp
 
 func take_damage(damage):
@@ -20,6 +29,7 @@ func take_damage(damage):
   _update_health_bar()
   
   if cur_hp <= 0:
+    get_node("/root/BattleScene").character_died(self)
     queue_free()
     
 func heal(amount):
@@ -33,3 +43,9 @@ func heal(amount):
 func _update_health_bar():
   health_bar.value = cur_hp
   health_text.text = str(cur_hp, "/", max_hp)
+
+func _on_character_begin_turn(character):
+  pass
+  
+func _on_character_end_turn(character):
+  pass
--- Courses/Build a Micro turn-Based RPG with Godot 4/TurnManager.gd
+++ Courses/Build a Micro turn-Based RPG with Godot 4/TurnManager.gd
@@ -9,6 +9,9 @@ var cur_char : Character
 
 var game_over : bool = false
 
+signal character_begin_turn(character)
+signal character_end_turn(character)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
   await get_tree().create_timer(0.5).timeout
@@ -22,7 +25,11 @@ func begin_next_turn():
   else:
     cur_char = player_char
   
+  emit_signal("character_begin_turn", cur_char)
+  
 func end_current_turn():
+  
+  emit_signal("character_end_turn", cur_char)
   
   await get_tree().create_timer(next_turn_delay).timeout
   
