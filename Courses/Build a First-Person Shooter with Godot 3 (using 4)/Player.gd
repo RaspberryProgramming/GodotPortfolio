68b8d94694d122172f83ce8aba82a15cd18878fa
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-21 11:39:54 -0400

    First Person Shooter - Lesson 8
    Adding Movement to player

--- Courses/Build a First-Person Shooter with Godot 3 (using 4)/Player.gd
+++ Courses/Build a First-Person Shooter with Godot 3 (using 4)/Player.gd
@@ -17,11 +17,49 @@ var jumpForce : float = 5.0
 @export var lookSensitivity : float = 10.0
 
 # vectors
-var vel : Vector3 = Vector3()
+#var vel : Vector3 = Vector3()
 var mouseDelta : Vector2 = Vector2()
 
 @onready var camera : Camera3D = $Camera3D
 @onready var muzzle : Node3D = $Camera3D/Muzzle
 
 func _physics_process(delta):
-  pass
+  # reset the x and z velocity
+  
+  var input = Vector2()
+  
+  # movement inputs
+  if Input.is_action_pressed("move_forward"):
+    input.y -= 1
+  
+  if Input.is_action_pressed("move_backward"):
+    input.y += 1
+    
+  if Input.is_action_pressed("move_left"):
+    input.x -= 1
+  
+  if Input.is_action_pressed("move_right"):
+    input.x += 1
+    
+  input = input.normalized()
+  
+  # get the forward and right directions
+  var forward = global_transform.basis.z
+  var right = global_transform.basis.x
+  
+  var relativeDir = (forward * input.y + right * input.x)
+  
+  # set the velocity
+  velocity.x = relativeDir.x * moveSpeed
+  velocity.z = relativeDir.z * moveSpeed
+  
+  # apply gravity
+  if not is_on_floor():
+    velocity.y -= gravity * delta
+  
+  # jumping
+  if Input.is_action_pressed("jump") and is_on_floor():
+    velocity.y = jumpForce
+    
+  move_and_slide()
+  
