5f41f462d69df8e88006d0d930b7f5750934f452
Camerin Figueroa <figueroa0609@protonmail.com>
2023-10-29 15:49:07 -0400

    Turn-Based Strategy Game - Lesson 8
    Added code for tile node

---
+++ Courses/Construct a Strategy Game with Godot 3 (using 4)/Tile.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var startTile : bool = false
+
+var hasBuilding : bool = false
+var canPlaceBuilding : bool = false
+
+# components
+@onready var highlight : Sprite2D = get_node("Hightlight")
+@onready var buildingIcon : Sprite2D = get_node("BuildingIcon")
+
+func _ready():
+  
+  add_to_group("Tiles")
+  
+func toggle_highlight(toggle):
+  
+  highlight.visible = toggle
+  canPlaceBuilding = toggle
+  
+func place_building(buildingTexture):
+  hasBuilding = true
+  buildingIcon.texture = buildingTexture
+
+func _on_input_event(viewport, event, shape_idx):
+  pass # Replace with function body.
