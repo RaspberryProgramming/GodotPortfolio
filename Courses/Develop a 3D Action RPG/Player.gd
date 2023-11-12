1a7f552e2e434b76707bab2eb09aee93a743d3ed
Camerin Figueroa <figueroa0609@protonmail.com>
2023-11-12 14:51:33 -0500

    ActionRPG - Lesson 9
    Adding jump action
    Adding gravity to player
    Applying velocity force to player

--- Courses/Develop a 3D Action RPG/Player.gd
+++ Courses/Develop a 3D Action RPG/Player.gd
@@ -11,7 +11,7 @@ var lastAttackTime : int = 0;
 
 var moveSpeed : float = 5.0;
 var jumpForce : float = 10.0;
-var gravity : float = 15.0;
+var gravity : float = 20.0;
 
 @onready var camera = get_node("CameraOrbit");
 @onready var attackRayCast = get_node("AttackRayCast");
@@ -39,4 +39,12 @@ func _physics_process(delta):
   velocity.x = dir.x * moveSpeed;
   velocity.z = dir.z * moveSpeed;
   
+  velocity.y -= gravity * delta; # Apply gravity to player's Y
+  
+  # Jump input action
+  if Input.is_action_pressed("jump") and is_on_floor():
+    velocity.y = jumpForce;
+  
+  # move along the current velocity
+  move_and_slide();
   
